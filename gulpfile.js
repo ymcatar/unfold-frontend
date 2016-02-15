1190386f64397dbcd3dec0ff736e507891e40498
Yiu Man Chi <ymcatar@gmail.com>
2016-02-15 23:30:15 +0800

    new packages, gulp now show jsx complete message

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var babelify = require('babelify');
 var source = require('vinyl-source-stream');
 var livereload = require('gulp-livereload');
 
+var gutil = require('gulp-util');
+
 var nodemon = require('gulp-nodemon');
 var watch = require('gulp-watch');
 
@@ -16,10 +18,11 @@ gulp.task('jsx', () => {
 			ignore: /(bower_components)|(node_modules)/
 		})
 		.bundle()
-		.on("error", err => { console.log("Error : " + err.message); })
+		.on("error", err => { gutil.log(err.message); })
 		.pipe(source('bundle.js'))
 		.pipe(gulp.dest('./dist'))
-		.pipe(livereload());
+		.pipe(livereload())
+		.on("end", () => { gutil.log("jsx completed."); });
 });
 
 // gulp.task('start', () => {
