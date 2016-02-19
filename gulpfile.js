31a57c5904389333ef01f7dcc668dda508bd24bb
Yiu Man Chi <ymcatar@gmail.com>
2016-02-20 01:28:46 +0800

    add system notification when jsx task completed

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,13 @@ var browserify = require('browserify');
 var babelify = require('babelify');
 
 var source = require('vinyl-source-stream');
-var livereload = require('gulp-livereload');
 
 var gutil = require('gulp-util');
 
 var nodemon = require('gulp-nodemon');
 var watch = require('gulp-watch');
+
+var notify = require('gulp-notify');
 
 gulp.task('jsx', () => {
 	browserify('./src/jsx/App.jsx', { debug: true, paths: ['./src/jsx'] })
@@ -21,8 +22,7 @@ gulp.task('jsx', () => {
 		.on("error", err => { gutil.log(err.message); })
 		.pipe(source('bundle.js'))
 		.pipe(gulp.dest('./dist'))
-		.pipe(livereload())
-		.on("end", () => { gutil.log("jsx completed."); });
+		.pipe(notify('jsx completed.'));
 });
 
 // gulp.task('start', () => {
@@ -31,8 +31,7 @@ gulp.task('jsx', () => {
 
 gulp.task('static', () => {
 	gulp.src(['./src/index.html', './src/main.css', './src/noembed.css'])
-		.pipe(gulp.dest('./dist'))
-		.pipe(livereload());
+		.pipe(gulp.dest('./dist'));
 });
 
 gulp.task('watch', () => {
