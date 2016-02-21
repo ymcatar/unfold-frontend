f1449405f52b36a9f6c0b41e93cc203b89af7888
Yiu Man Chi <ymcatar@gmail.com>
2016-02-21 17:30:15 +0800

    added docus handler

--- src/jsx/right/common/Avatar.jsx
+++ src/jsx/right/common/Avatar.jsx
@@ -20,7 +20,7 @@ export default class Avatar extends React.Component {
 		return (
 			<div style={this.props.style}>
 				<OverlayTrigger
-					trigger={"hover"}
+					trigger={["hover", "focus"]}
 					placement="bottom"
 					overlay={
 						<Popover
