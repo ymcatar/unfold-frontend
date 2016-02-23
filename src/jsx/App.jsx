3330aa35b322f9b5d9ae5d31f21ad54b0c320cc4
Yiu Man Chi <ymcatar@gmail.com>
2016-02-23 16:15:12 +0800

    preparing to add contributor view

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -5,10 +5,13 @@ import $ from 'jquery-browserify';
 import {Router, Route, Link, IndexRoute} from 'react-router';
 
 import ReaderView from 'views/ReaderView.jsx';
+import ContributorView from 'views/ContributorView.jsx';
 
 ReactDOM.render((
     <Router>
-        <Route path='/' component={ReaderView}>
+        <Route path='/'>
+            <Route path='contributor' component={ContributorView} />
+            <IndexRoute path='*' component={ReaderView} />
         </Route>
     </Router>
 ), document.getElementById('main'));
---
+++ src/jsx/views/ContributorView.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import MediaQuery from 'react-responsive';
+
+import Colors from 'config/Colors.jsx';
+import Placeholder from 'config/Placeholder.jsx';
+
+export default class ContributorView extends React.Component {
+
+	constructor() {
+		super();
+	}
+
+	render() {
+		return (
+			<div>
+				Contributor assemble!
+			</div>
+		);
+	}
+}
