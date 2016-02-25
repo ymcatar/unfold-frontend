8648aabbc0e53740d21b77e82787b4d0631ed6d9
Yiu Man Chi <ymcatar@gmail.com>
2016-02-25 18:07:17 +0800

    switching to history api for react-router

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ var routes = require('./routes/index.js');
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res, next) => {
+app.get('*', (req, res, next) => {
 	res.sendFile(path.resolve(__dirname, './dist/index.html'));
 });
 
 app.use('/', routes);
 
 app.listen(3000, () => {
-	console.log('Server running at port 3000.');	
+	console.log('Server running at port 3000.');
 });
 
 module.exports = app;
--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import createBrowserHistory from 'history/lib/createBrowserHistory';
+
+const history = createBrowserHistory({ queryKey: false });
+
 import $ from 'jquery-browserify';
-import {Router, Route, Link, IndexRoute} from 'react-router';
+import {Router, Route, Link, IndexRoute, browserHistory} from 'react-router';
 
 import ReaderView from 'views/ReaderView.jsx';
 import ContributorView from 'views/ContributorView.jsx';
 
 ReactDOM.render((
-    <Router>
+    <Router history={history}>
         <Route path='/'>
             <Route path='contributor' component={ContributorView} />
             <IndexRoute component={ReaderView} />
