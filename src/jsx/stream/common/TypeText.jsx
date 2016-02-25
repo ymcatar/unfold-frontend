4bc00d3efa1a5d6d3965d5a95ca885495d8580fe
Yiu Man Chi <ymcatar@gmail.com>
2016-02-25 23:29:02 +0800

    switching from markdown to html

--- src/jsx/stream/common/TypeText.jsx
+++ src/jsx/stream/common/TypeText.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import markdown from 'common/Markdown.js';
 
 export default class TypeText extends React.Component {
     render() {
         let data = this.props.data || '';
         return (
             <div>
-                <p dangerouslySetInnerHTML={{__html: markdown(data)}} />
+                <p dangerouslySetInnerHTML={{__html: data}} />
             </div>
         );
     }
