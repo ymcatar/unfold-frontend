1bc9d17ff5b346398b40a3a8fe2812e4bfa4b860
Yiu Man Chi <ymcatar@gmail.com>
2016-02-29 10:11:20 +0800

    added mail sending

--- src/jsx/header/ReaderHeader.jsx
+++ src/jsx/header/ReaderHeader.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import {Navbar, Nav, NavItem} from 'react-bootstrap';
+import { Navbar, Nav, NavItem, Modal, Button, Input } from 'react-bootstrap';
+import _ from 'lodash';
 
 import Colors from 'config/Colors.jsx';
 import { selectFilter, reportFilter, scrollToTop } from '../actions/stream';
@@ -21,55 +22,121 @@ const styles = {
 class ReaderHeader extends React.Component {
     constructor(props) {
         super(props);
-        this.handleFilter = this.handleFilter.bind(this);
+        this.state = {showModal: false};
+        this.elm = {};
+        _.bindAll(this, [
+            'handleNavClick',
+            'handleFilter',
+            'hideModal',
+            'showModal',
+            'handleSubmit'
+        ]);
     }
 
     handleFilter(key) {
+        this.props.handleFilter(key);
+    }
+
+    handleNavClick(key) {
         switch (key) {
-            case 'important':
-                this.props.handleFilter('important');
+            case 'top':
+                this.props.handleBackToTop();
                 break;
-            case 'reliable':
-                this.props.handleFilter('reliable');
-                break;
-            case 'all':
-                this.props.handleFilter('all');
+            case 'mail':
+                this.showModal();
                 break;
         }
     }
 
+    hideModal() {
+        this.setState({showModal: false});
+    }
+
+    showModal() {
+        this.setState({showModal: true});
+    }
+
+    handleSubmit() {
+        this.hideModal();
+        let output = {
+            target: this.elm.target.getValue(),
+            content: this.elm.content.getValue()
+        };
+        console.log(output);
+    }
+
     render() {
         return (
-            <Navbar style={styles.main} fixedTop={true} fluid={true}>
-                <Navbar.Toggle>
-                    <i className="fa fa-bars" />
-                    &nbsp;Toggle
-                </Navbar.Toggle>
-                <Navbar.Collapse>
-                    <Navbar.Header>
-                        <Navbar.Text>
-                            <img src="res/logo.png" style={styles.logo}/>
-                        </Navbar.Text>
-                    </Navbar.Header>
-                    <Nav onSelect={this.handleFilter} activeKey={this.props.filter}>
-                        <NavItem eventKey={'all'} href="#">
-                            All
-                        </NavItem>
-                        <NavItem eventKey={'important'} href="#">
-                            Important
-                        </NavItem>
-                        <NavItem eventKey={'reliable'} href="#">
-                            Reliable
-                        </NavItem>
-                    </Nav>
-                    <Nav pullRight onSelect={this.props.handleBackToTop}>
-                        <NavItem eventKey={'top'} href="#">
-                            <i className="fa fa-chevron-circle-up" />
-                            &nbsp;Top
-                        </NavItem>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
+            <div>
+                <Navbar style={styles.main} fixedTop={true} fluid={true}>
+                    <Navbar.Toggle>
+                        <i className="fa fa-bars" />
+                        &nbsp;Toggle
+                    </Navbar.Toggle>
+                    <Navbar.Collapse>
+                        <Navbar.Header>
+                            <Navbar.Text>
+                                <img src="res/logo.png" style={styles.logo}/>
+                            </Navbar.Text>
+                        </Navbar.Header>
+                        <Nav onSelect={this.handleFilter} activeKey={this.props.filter}>
+                            <NavItem eventKey={'all'} href="#">
+                                All
+                            </NavItem>
+                            <NavItem eventKey={'important'} href="#">
+                                Important
+                            </NavItem>
+                            <NavItem eventKey={'reliable'} href="#">
+                                Reliable
+                            </NavItem>
+                        </Nav>
+                        <Nav pullRight onSelect={this.handleNavClick}>
+                            <NavItem eventKey={'mail'} href="#">
+                                <i className="fa fa-envelope" />
+                            </NavItem>
+                            <NavItem eventKey={'top'} href="#">
+                                <i className="fa fa-chevron-circle-up" />
+                                &nbsp;Top
+                            </NavItem>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+                <Modal
+                    show={this.state.showModal}
+                    onHide={this.hideModal}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Send Message To Contributor</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Input
+                            ref={x => {this.elm.target = x;}}
+                            type="select"
+                            label="Send to"
+                            multiple>
+                            {
+                                this.props.contributor.map(o => (
+                                    <option value={o.id}>
+                                        {o.name}
+                                    </option>
+                                ))
+                            }
+                        </Input>
+                        <Input
+                            ref={x => {this.elm.content = x;}}
+                            type="textarea"
+                            style={{height: 300}}
+                            label="Mail content" />
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button onClick={this.hideModal}>Cancel</Button>
+                        <Button
+                            onClick={this.handleSubmit}
+                            bsStyle="primary">
+                            Send
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
         );
     }
 }
@@ -77,7 +144,8 @@ class ReaderHeader extends React.Component {
 export default connect(
     function stateToProps(state) {
         return {
-            filter: state.stream.filter
+            filter: state.stream.filter,
+            contributor: state.event.contributors
         };
     },
     function dispatchToProps(dispatch) {
