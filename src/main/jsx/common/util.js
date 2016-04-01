72ea1206415d2dd401ade69ce777c77ab0eb6829
Yiu Man Chi <ymcatar@gmail.com>
2016-04-01 20:27:00 +0800

    refactor: inViewport function separated

--- src/main/jsx/common/util.js
+++ src/main/jsx/common/util.js
@@ -2,7 +2,6 @@ export function isElementInViewport (el) {
     if (!el)
         return false;
     var rect = el.getBoundingClientRect();
-    console.log(window.innerHeight, rect.bottom);
     return (
         (rect.top >= (window.innerHeight / 5 || document.documentElement.clientHeight / 5) &&
         rect.bottom <= (window.innerHeight * 3 / 4 || document.documentElement.clientHeight * 3 / 4))
