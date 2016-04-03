559063df0d27ab802904ef73596d559bf5d5f853
Yiu Man Chi <ymcatar@gmail.com>
2016-04-03 15:28:26 +0800

    fix: role list online count hidden

--- src/main/jsx/sidebar/event/common/RoleList.jsx
+++ src/main/jsx/sidebar/event/common/RoleList.jsx
@@ -6,11 +6,12 @@ import AvatarList from './AvatarList.jsx';
 export default class RoleList extends React.Component {
     render() {
         let total = this.props.data.length;
-        let online = this.props.data.filter(curr => curr.online).length;
+        // let online = this.props.data.filter(curr => curr.online).length;
         let { header } = this.props;
 
+        //  (${online}/${total})
         return (
-            <CollapseBox header={`${header} (${online}/${total})`} defaultCollapsed={false} >
+            <CollapseBox header={`${header}`} defaultCollapsed={false} >
                 <AvatarList data={this.props.data} />
             </CollapseBox>
         );
