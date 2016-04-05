514c29d10f6f88b38e98444165d8e1d5953e2f6c
Yiu Man Chi <ymcatar@gmail.com>
2016-04-05 22:58:53 +0800

    fix: bottom embed not expanding

--- src/main/jsx/common/util.js
+++ src/main/jsx/common/util.js
@@ -4,6 +4,7 @@ export function isElementInViewport (el) {
     var rect = el.getBoundingClientRect();
     return (
         (rect.top >= (window.innerHeight / 5 || document.documentElement.clientHeight / 5) &&
-        rect.bottom <= (window.innerHeight * 3 / 4 || document.documentElement.clientHeight * 3 / 4))
+        rect.bottom <= (window.innerHeight * 3 / 4 || document.documentElement.clientHeight * 3 / 4)) ||
+        (rect.bottom > window.innerHeight - 200)
     );
 }
